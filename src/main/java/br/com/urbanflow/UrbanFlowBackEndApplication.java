db8a79d5ac35c5ff9322b25e235087cde628d35a
atusoliverss <batistaatus131@gmail.com>
2025-01-30 11:15:11 -0300

    Projeto Inicial BackEnd

---
+++ src/main/java/br/com/urbanflow/UrbanFlowBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.urbanflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanFlowBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanFlowBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/urbanflow/UrbanFlowBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.urbanflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanFlowBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
